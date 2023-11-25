0f464b57404edfbf4620c10ea8deaed71ac56655
ddmv19 <moronvillanuevadiego@outlook.com>
2023-11-25 03:16:38 -0500

    Add second challenge

---
+++ challenges/chall-02.js
@@ -0,0 +1,25 @@
+
+const RESOLVE_SYMBOLS = {
+  "#": (num) => num + 1,
+  "@": (num) => num - 1,
+  "*": (num) => num * num,
+  "&": (num) => num,
+}
+
+const INITIAL_VALUE = 0
+
+const resolveSymbols = (symbols = '') => {
+  let output = ''
+  let currentValue = INITIAL_VALUE
+  for (const symbol of symbols) {
+    if(symbol === '&') output += RESOLVE_SYMBOLS[symbol](currentValue)
+    // output += RESOLVE_SYMBOLS[symbol](currentValue)
+    currentValue = RESOLVE_SYMBOLS[symbol](currentValue)
+  }
+  return output
+}
+
+
+const input = '&###@&*&###@@##@##&######@@#####@#@#@#@##@@@@@@@@@@@@@@@*&&@@@@@@@@@####@@@@@@@@@#########&#&##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@&'
+
+console.log(resolveSymbols(input))
